60f7dee5a448e57b33dec97fbdfb1a6ba8a2f6e8
Adonis Cruz <adonisacruz0@gmail.com>
2023-08-01 08:20:10 -0400

    adding a default endpoint/path operation

--- main.py
+++ main.py
@@ -25,3 +25,8 @@ app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(votes.router)
+
+
+@app.get('/')
+def entry():
+    return {"message": "Hello Heroku!"}
